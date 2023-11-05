695547a3ba7fb4a0391c0759c93039517d68c937
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-05 12:27:53 +0000

    done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,41 @@
-
-
+import { Box ,Button,Center,Input,Text, VStack} from '@chakra-ui/react'
+import { useState } from 'react';
 
 function App() {
+   const [text,setText]=useState('')
+   const [loading,setLoading]=useState(false)
+ const handleSubmit=()=>{
+  setLoading(true)
+
+  if ('speechSynthesis' in window) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    synth.speak(utterance);
+  } else {
+    alert('Text-to-speech is not supported in your browser.');
+  }
+  setLoading(false)
+
+
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Box w="100vw" h="100vh">
+     <Center  h="100vh" >
+      <VStack>
+      <Text fontSize='xl' color='blue.400' mb='10px' fontFamily={"Arvo"}>Enter Text To Speech Converter App</Text>
+      <Box h="100px" w="400px" boxShadow='dark-lg' rounded={'xl'} p={"5px"}>
+      <Input placeholder='Enter Text' value={text} onChange={(e)=>setText(e.target.value)}/>
+      <Box w="100%" >
+        <Center h="50px">
+      <Button colorScheme='blue' isLoading={loading} onClick={()=>handleSubmit()}>Click Here</Button>
+      </Center>
+      </Box>
+    
+      </Box>
+      </VStack>
+       </Center>
+    </Box>
   );
 }
 
